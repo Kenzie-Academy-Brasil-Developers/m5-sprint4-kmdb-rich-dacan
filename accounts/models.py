02f6a047df01275899b67f318dd5ac8026142493
Richard Cândido <rich_dacan@protonmail.com>
2022-08-22 11:57:51 -0300

    feat :sparkles: created model Accounsts extended of AbstractUser

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class Accounts(AbstractUser):
-    ...
+    birthdate = models.DateField()
+    bio = models.TextField()
+    is_critic = models.BooleanField(default=False)
+    updated_at = models.DateTimeField()
